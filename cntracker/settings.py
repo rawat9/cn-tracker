e22bb4a74dd05cfe0ee049d2712f5f0bd34e703f
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-06-22 00:11:29 +0100

    update settings.py

--- cntracker/settings.py
+++ cntracker/settings.py
@@ -132,11 +132,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cntracker/static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
