fc5dbbdcc49758521a29c1aa9c93dd5ddbd1ba02
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-06-21 16:06:48 +0100

    update settings.py

--- cntracker/settings.py
+++ cntracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,4 +154,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('CN_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('CN_PASSWORD')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
