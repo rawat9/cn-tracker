99908dfff99e38e7220d5787d716d60c2370f1df
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-07-08 23:59:37 +0100

    ✓ activityform

--- activityform/views.py
+++ activityform/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.shortcuts import redirect
 from .models import Activity
 from webpages.models import Project, Topic
@@ -15,11 +16,16 @@ def activity_form(request):
         sensei_name = request.POST['sensei_name']
         comment = request.POST['comment']
 
-    activity_form = Activity(user_id=User.objects.get(id=user_id), 
+        if Activity.objects.filter(user_id=user_id).exists() and Activity.objects.filter(project_id=project_id).exists():
+            messages.warning(request, 'Record already exists')
+            return redirect('scorecard')
+        else:
+            activity_form = Activity(user_id=User.objects.get(id=user_id), 
                             topic_id=Topic.objects.get(topic_id=topic_id),
                             project_id=Project.objects.get(project_id=project_id), 
                             rating=rating, 
                             sensei_name=sensei_name, 
                             comment=comment)
-    activity_form.save()
-    return redirect('scorecard')
+            activity_form.save()
+            messages.success(request, 'Record Saved Successfully!')
+            return redirect('scorecard')
