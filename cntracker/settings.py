ef14134a3b4193333887ec3e0bfcd1848e4c6838
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-08-31 13:16:21 +0100

    + disable 2FA

--- cntracker/settings.py
+++ cntracker/settings.py
@@ -152,7 +152,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-OTP_TOTP_ISSUER = 'Code Ninjas North Finchley'
+# OTP_TOTP_ISSUER = 'Code Ninjas North Finchley'
 
 
 LOGIN_URL = 'login'
