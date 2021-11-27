037135cd674429bd7a20c59052586bd98925fe45
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-11-27 12:47:19 +0000

    + date_created > list_display {Activities}

--- activityform/admin.py
+++ activityform/admin.py
@@ -7,7 +7,7 @@ from .models import Activity
 class ActivityModel(ImportExportModelAdmin):
     resource_class = ActivityResource
 
-    list_display = ('id', 'user_id', 'project_id', 'topic_id')
+    list_display = ('id', 'user_id', 'project_id', 'topic_id', 'date_created')
     list_display_links = ('id', 'user_id',)
     list_filter = ('date_created', 'topic_id', 'user_id')
     readonly_fields = ('date_created',)
