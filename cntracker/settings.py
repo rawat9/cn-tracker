da855997655548c299938e232211943a8d60d70b
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-01-12 17:29:11 +0000

    temp DEBUG=True

--- cntracker/settings.py
+++ cntracker/settings.py
@@ -30,7 +30,8 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
  
 ALLOWED_HOSTS = ['127.0.0.1', 'cn-north-finchley.herokuapp.com']
 
