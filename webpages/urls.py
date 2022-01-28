13f5b8ebf68674763c8c29e018af9163f3004cd7
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-01-28 23:51:55 +0000

    + leaderboard ⭐️

--- webpages/urls.py
+++ webpages/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('users', views.users, name="users"),
+    path('user/<int:pk>', views.user_profile, name="user"),
     path('badges', views.badges, name="badges"),
+    path('leaderboard', views.leaderboard, name="leaderboard"),
 ]
